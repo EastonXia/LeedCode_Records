23ce480d1b4538d6f3c1f64be968fc0c71564356
EastonXia <xia.yiming@iwhalecloud.com>
2022-04-12 11:48:39 +0800

    添加217

---
+++ LeetCode217.mjs
@@ -0,0 +1,48 @@
+/**
+  LeetCode217---存在重复元素
+  
+  给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ 
+  示例 1：
+  输入：nums = [1,2,3,1]
+  输出：true
+
+  示例 2：
+  输入：nums = [1,2,3,4]
+  输出：false
+
+  示例 3：
+  输入：nums = [1,1,1,3,3,4,3,2,4,2]
+  输出：true
+
+  提示：
+    1 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const set = new Set();
+
+  for (let item of nums) {
+    if (set.has(item)) {
+      return true;
+    } else {
+      set.add(item);
+    }
+  }
+
+  return false;
+};
+
+function main() {
+  console.log(containsDuplicate([1, 2, 3, 1]));
+  console.log(containsDuplicate([1, 2, 3, 4]));
+  console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+}
+
+main();
